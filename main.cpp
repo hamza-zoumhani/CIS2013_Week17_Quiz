bdb56f8accd946923fd7517fc462ace6813336d9
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-05-08 18:20:01 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,16 +9,16 @@ double mpg(int m, int l);
 int main(){
 	
 	char choice;
-	double litter, miles;
+	double litter, mile;
 	
 	do{
 		cout << endl;
 		cout <<"Number of Liters consumed: ";
 		cin >> litter;
 		cout <<"Number of miles traveled: ";
-		cin >> miles;
+		cin >> mile;
 		cout << "Number of miles per gallon the car delivered: " ;
-		cout << mpg(miles, litter) << endl;
+		cout << mpg(mile, litter) << endl;
 		cout << "Again? (y/n)";
 		cin >> choice;
 	}while(choice =='y');
